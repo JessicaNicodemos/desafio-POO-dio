394afa1ac33f3ecf0ee00785488a86b3a7287b83
Jessica <jessicanicodemos@gmail.com>
2022-11-26 22:46:29 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.dominio.Conteudo;
-import br.com.dio.dominio.Curso;
-import br.com.dio.dominio.Mentoria;
+import br.com.dio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -24,8 +22,48 @@ public class Main {
 
         Conteudo conteudo = new Curso();
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJessica = new Dev();
+        devJessica.setNome("Jéssica");
+        devJessica.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devJessica.getConteudosIncritos());
+
+        devJessica.progredir();
+        devJessica.progredir();
+
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos" + devJessica.getConteudosIncritos());
+        System.out.println("Conteudos Concluidos" + devJessica.getConteudosConcluidos());
+        System.out.println("XP: " + devJessica.calcularTotalXp());
+
+        System.out.println("----------");
+
+        Dev devLeo = new Dev();
+        devLeo.setNome("Leo");
+        devLeo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devLeo.getConteudosIncritos());
+
+        devLeo.progredir();
+        devLeo.progredir();
+        devLeo.progredir();
+
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos" + devLeo.getConteudosIncritos());
+        System.out.println("Conteudos Concluidos" + devLeo.getConteudosConcluidos());
+        System.out.println("XP: " + devLeo.calcularTotalXp());
+
+
+
+
     }
 }
